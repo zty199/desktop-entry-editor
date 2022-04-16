598768b79b9b31e6878fe50b0f274ae9179bface
Alvaro Samudio <alvarosamudio@criptext.com>
2022-04-16 08:46:26 -0500

    add Spanish translation

---
+++ src/translations/desktop-entry-editor_es_ES.ts
@@ -0,0 +1,305 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>About</name>
+    <message>
+        <location filename="../about.cpp" line="7"/>
+        <source>Desktop Entry Editor</source>
+        <translation>Editor de Escritorio</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="8"/>
+        <source>Version: %1</source>
+        <translation>Version: %1</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="22"/>
+        <source>A simple GUI tool to create and edit desktop files</source>
+        <translation>Una sencilla herramienta de interfaz gráfica para crear y editar archivos de escritorio</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="23"/>
+        <source>Desinged for Deepin</source>
+        <translation>Diseñado para Deepin</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="24"/>
+        <source>Powered by Deepin Community</source>
+        <translation>Desarrollado por Deepin Community</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="25"/>
+        <source>Spark Developers</source>
+        <translation>Desarrolladores Spark</translation>
+    </message>
+    <message>
+        <location filename="../about.cpp" line="28"/>
+        <source>Published under GPL V3</source>
+        <translation>Publicado bajo GPL V3</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.cpp" line="31"/>
+        <source>New</source>
+        <translation>Nuevo</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="32"/>
+        <location filename="../mainwindow.cpp" line="51"/>
+        <source>Open</source>
+        <translation>Abrir</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="33"/>
+        <source>Save</source>
+        <translation>Guardar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="34"/>
+        <location filename="../mainwindow.cpp" line="461"/>
+        <source>Save As</source>
+        <translation>Guardar como</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="35"/>
+        <source>Settings</source>
+        <translation>Ajustes</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="36"/>
+        <source>Theme</source>
+        <translation>Tema</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="37"/>
+        <source>Light theme</source>
+        <translation>Tema claro</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="38"/>
+        <source>Dark theme</source>
+        <translation>Tema oscuro</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="39"/>
+        <source>System theme</source>
+        <translation>Tema del sistema</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="40"/>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="41"/>
+        <source>Exit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="175"/>
+        <source>Name</source>
+        <translation>Nombre</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="177"/>
+        <source>Exec</source>
+        <translation>Exec</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="179"/>
+        <source>Comment</source>
+        <translation>Comentario</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="181"/>
+        <source>MimeType</source>
+        <translation>MimeType</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="183"/>
+        <source>Categories</source>
+        <translation>Categorías</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="186"/>
+        <source>Run in Terminal</source>
+        <translation>Ejecutar en Terminal</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="188"/>
+        <source>Not Display in Launcher</source>
+        <translation>No mostrar en el Lanzador</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="210"/>
+        <source>Desktop Entry Editor</source>
+        <translation>Editor de Escritorio</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="222"/>
+        <source>Click to choose icon</source>
+        <translation>Haga clic para elegir el icono</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="226"/>
+        <source>Name to display</source>
+        <translation>Nombre a mostrar</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="228"/>
+        <source>Comment of this .desktop</source>
+        <translation>Comentario de este .desktop</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="230"/>
+        <source>Choose a binary</source>
+        <translation>Elija un binario</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="232"/>
+        <source>Supported MIME type(s)</source>
+        <translation>MIME type(s) Soportado</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="239"/>
+        <location filename="../mainwindow.cpp" line="322"/>
+        <source>- Please Select -</source>
+        <translation>- Por favor, seleccione -</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="240"/>
+        <location filename="../mainwindow.cpp" line="328"/>
+        <source>Network</source>
+        <translation>Internet</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="241"/>
+        <location filename="../mainwindow.cpp" line="329"/>
+        <source>Chat</source>
+        <translation>Mensajeria</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="242"/>
+        <location filename="../mainwindow.cpp" line="330"/>
+        <source>Music</source>
+        <translation>Música</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="243"/>
+        <location filename="../mainwindow.cpp" line="331"/>
+        <source>Video</source>
+        <translation>Vídeo</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="244"/>
+        <location filename="../mainwindow.cpp" line="332"/>
+        <source>Graphics</source>
+        <translation>Gráficos</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="245"/>
+        <location filename="../mainwindow.cpp" line="333"/>
+        <source>Game</source>
+        <translation>Juegos</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="246"/>
+        <location filename="../mainwindow.cpp" line="334"/>
+        <source>Office</source>
+        <translation>Ofimática</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="247"/>
+        <location filename="../mainwindow.cpp" line="335"/>
+        <source>Translation</source>
+        <translation>Lectura</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="248"/>
+        <location filename="../mainwindow.cpp" line="336"/>
+        <source>Development</source>
+        <translation>Desarrollo</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="249"/>
+        <location filename="../mainwindow.cpp" line="337"/>
+        <source>System</source>
+        <translation>Sistema</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="250"/>
+        <location filename="../mainwindow.cpp" line="338"/>
+        <source>Others</source>
+        <translation>Otros</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="451"/>
+        <source>Select file</source>
+        <translation>Seleccionar archivo</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="451"/>
+        <source>All Files (*)</source>
+        <translation>Todos los archivos (*)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="461"/>
+        <source>Desktop Entry Files (*.desktop)</source>
+        <translation>Archivos de entrada al escritorio (*.desktop)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="581"/>
+        <source>Select icon</source>
+        <translation>Seleccione el icono</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="581"/>
+        <source>Image Files (*.png *.jpg *.svg *.xpm)</source>
+        <translation>Archivos de imagen (*.png *.jpg *.svg *.xpm)</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="677"/>
+        <source>is saved.</source>
+        <translation>está guardado.</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="677"/>
+        <source>Show in file manager?</source>
+        <translation>¿Mostrar en el gestor de archivos?</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="12"/>
+        <source>Desktop Entry Editor</source>
+        <translation>Editor de Escritorio</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <location filename="../settings.ui" line="20"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="70"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="83"/>
+        <source>Opacity</source>
+        <translation>Opacidad</translation>
+    </message>
+    <message>
+        <location filename="../settings.ui" line="96"/>
+        <source>Blur</source>
+        <translation>Desenfoque</translation>
+    </message>
+</context>
+</TS>
